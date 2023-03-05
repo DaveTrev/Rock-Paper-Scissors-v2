86007e9408377c53d2910961c3d6896b01f9636e
DaveTrev <trevaskisdavid@gmail.com>
2023-03-05 07:50:18 +0000

    win lose draw conditions added to js file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,17 +8,36 @@ const rock_div = document.getElementById("r")
 const paper_div = document.getElementById("p")
 const scissors_div = document.getElementById("s")
 
+// computer choice is an array of choices via random number generation
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
     const randomNumber = (Math.floor(Math.random() * 3));
     return choices[randomNumber];
 }                        
 
+// The logic of game, win, lose, draw conditions
 function game(userChoice) {
-    const computerChoice = getComputerChoice
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+      case "rs":
+      case "pr":
+      case "sp":
+        console.log("User Wins");
+        break;
+      case "rp":
+      case "ps":
+      case "sr":
+            console.log("User Loses!");
+            break;
+      case "rr":
+      case "pp":
+      case "ss":
+        console.log("Its a draw");
+        break;
+
+    }
 
 }
-
 
 // event listeners to create actions on r/p/s images
 function main() {

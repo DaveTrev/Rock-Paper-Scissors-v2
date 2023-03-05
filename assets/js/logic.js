ca5241c9de24885a14b911d93b1dbaba7d2b4cf2
DaveTrev <trevaskisdavid@gmail.com>
2023-03-05 10:32:29 +0000

    removed unused var scoreBoard_div ran through jshint found unused

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,11 +3,10 @@ let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result > p")
-const rock_div = document.getElementById("r")
-const paper_div = document.getElementById("p")
-const scissors_div = document.getElementById("s")
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
 // Setting up computer choice. using math.random to loop through an array to return a value
 function getComputerChoice() {
